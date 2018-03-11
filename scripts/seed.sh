0d35a13a6c2d279f3432e32efbd5522bbd602f57
PhantomOverride <PhantomOverride@users.noreply.github.com>
2018-03-11 15:54:38 +0100

    Update seed.sh

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 echo "mKassa - Setting up products... "
 
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"C0la","price":10,"imageUrl":"/assets/products/cola.png","category":"None"}'  1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"C0la 0x00","price":10,"imageUrl":"/assets/products/colazero.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Fanta Extc","price":10,"imageUrl":"/assets/products/fantaexotic.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Cola","price":10,"imageUrl":"/assets/products/cola.png","category":"None"}'  1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Cola Zero","price":10,"imageUrl":"/assets/products/colazero.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Fanta Exotic","price":10,"imageUrl":"/assets/products/fantaexotic.png","category":"None"}' 1>/dev/null
 curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Fanta","price":10,"imageUrl":"/assets/products/fanta.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Mtn Dew","price":10,"imageUrl":"/assets/products/mntdew.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Raml`o`sa","price":10,"imageUrl":"/assets/products/ramlosa.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Rmlsa Ctr","price":10,"imageUrl":"/assets/products/ramlosacitrus.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Pwrkng#","price":15,"imageUrl":"/assets/products/powerking.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"K4ffe","price":10,"imageUrl":"/assets/products/kaffe.gif","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"K4ff3b4nd","price":40,"imageUrl":"/assets/products/kaffekort.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Billys Org","price":20,"imageUrl":"/assets/products/billys.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Mountain Dew","price":10,"imageUrl":"/assets/products/mntdew.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Ramlösa","price":10,"imageUrl":"/assets/products/ramlosa.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Ramlösa Citrus","price":10,"imageUrl":"/assets/products/ramlosacitrus.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Powerking","price":15,"imageUrl":"/assets/products/powerking.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Kaffe","price":10,"imageUrl":"/assets/products/kaffe.gif","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Kaffeband","price":40,"imageUrl":"/assets/products/kaffekort.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Billys","price":20,"imageUrl":"/assets/products/billys.png","category":"None"}' 1>/dev/null
 curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Billys Veg","price":20,"imageUrl":"/assets/products/billysveg.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"G0rbys","price":15,"imageUrl":"/assets/products/gorbys.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"AEpple","price":5,"imageUrl":"/assets/products/apple.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"T0ast","price":20,"imageUrl":"/assets/products/toast.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"bnn_sKid","price":1,"imageUrl":"/assets/products/skids.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Snixers","price":5,"imageUrl":"/assets/products/snickers.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Gott&etc","price":15,"imageUrl":"/assets/products/gottoblandat.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"5_gdshband","price":5,"imageUrl":"/assets/products/sample.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"20_ptr*","price":5,"imageUrl":"/assets/products/surapatroner.png","category":"None"}' 1>/dev/null
-curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"c_okie","price":10,"imageUrl":"/assets/products/sample.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Gorbys","price":15,"imageUrl":"/assets/products/gorbys.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Äpple","price":5,"imageUrl":"/assets/products/apple.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Toast","price":20,"imageUrl":"/assets/products/toast.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Banana Skids","price":1,"imageUrl":"/assets/products/skids.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Snickers","price":5,"imageUrl":"/assets/products/snickers.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Gott & Blandat","price":15,"imageUrl":"/assets/products/gottoblandat.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"5st Godishalsband","price":5,"imageUrl":"/assets/products/sample.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"20st Patroner","price":5,"imageUrl":"/assets/products/surapatroner.png","category":"None"}' 1>/dev/null
+curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Cookie","price":10,"imageUrl":"/assets/products/sample.png","category":"None"}' 1>/dev/null
 curl -XPOST 'http://localhost:3000/product' -H "Content-Type: application/json" --data '{"name":"Datorplats","price":200,"imageUrl":"/assets/products/sample.png","category":"None"}' 1>/dev/null
 
